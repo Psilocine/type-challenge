066c221464ac87dcc2fa403e9bff900cd6495486
PsiloLau <psilolau@foxmail.com>
2022-03-23 16:14:53 +0800

    docs: daily type learning

--- 2_medium/20_Promise.all.ts
+++ 2_medium/20_Promise.all.ts
@@ -40,7 +40,9 @@ type cases = [
 
 /* _____________ Your Code Here _____________ */
 
-declare function PromiseAll(values: any): any;
+declare function PromiseAll<T extends any[]>(
+  values: readonly [...T]
+): Promise<{ [K in keyof T]: T[K] extends Promise<infer R> ? R : T[K] }>;
 
 /* _____________ Further Steps _____________ */
 /*
