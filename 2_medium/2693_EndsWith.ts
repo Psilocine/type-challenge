0e3d03070f92bb17bf84837189398cd51570d404
PsiloLau <psilolau@foxmail.com>
2022-03-30 20:12:04 +0800

    docs: daily type learning

---
+++ 2_medium/2693_EndsWith.ts
@@ -0,0 +1,37 @@
+/*
+  2693 - EndsWith
+  -------
+  by jiangshan (@jiangshanmeta) #medium #template-literal
+  
+  ### Question
+  
+  Implement `EndsWith<T, U>` which takes two exact string types and returns whether `T` ends with `U`
+  
+  > View on GitHub: https://tsch.js.org/2693
+*/
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect, ExpectFalse, NotEqual } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<EndsWith<"abc", "bc">, true>>,
+  Expect<Equal<EndsWith<"abc", "abc">, true>>,
+  Expect<Equal<EndsWith<"abc", "d">, false>>
+];
+
+/* _____________ Original Code Here _____________ */
+
+// type EndsWith<T extends string, U extends string> = any;
+
+/* _____________ Your Code Here _____________ */
+
+type EndsWith<T extends string, U extends string> = T extends `${infer any}${U}`
+  ? true
+  : false;
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2693/answer
+  > View solutions: https://tsch.js.org/2693/solutions
+  > More Challenges: https://tsch.js.org
+*/
