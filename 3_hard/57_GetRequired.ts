c9491d5257f856795946b84ab7bcfdb8b4c0ef02
PsiloLau <psilolau@foxmail.com>
2022-04-08 15:51:01 +0800

    docs: daily type learning

---
+++ 3_hard/57_GetRequired.ts
@@ -0,0 +1,45 @@
+/*
+  57 - Get Required
+  -------
+  by Zheeeng (@zheeeng) #hard #utils #infer
+  
+  ### Question
+  
+  Implement the advanced util type `GetRequired<T>`, which remains all the required fields
+  
+  For example
+  
+  ```ts
+  type I = GetRequired<{ foo: number, bar?: string }> // expected to be { foo: number }
+  ```
+  
+  > View on GitHub: https://tsch.js.org/57
+*/
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<GetRequired<{ foo: number; bar?: string }>, { foo: number }>>,
+  Expect<Equal<GetRequired<{ foo?: number; bar?: string }>, {}>>,
+  Expect<
+    Equal<GetRequired<{ foo: undefined; bar?: undefined }>, { foo: undefined }>
+  >
+];
+
+/* _____________ Original Code Here _____________ */
+
+// type GetRequired<T> = any;
+
+/* _____________ Your Code Here _____________ */
+
+type GetRequired<T> = {
+  [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K];
+};
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/57/answer
+  > View solutions: https://tsch.js.org/57/solutions
+  > More Challenges: https://tsch.js.org
+*/
