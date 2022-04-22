07b97ee14c6105a47846d98db5dd491f30c1595d
PsiloLau <psilolau@foxmail.com>
2022-04-22 18:17:05 +0800

    docs: daily type learning

---
+++ 3_hard/956_DeepPick.ts
@@ -0,0 +1,105 @@
+/*
+  956 - DeepPick
+  -------
+  by hiroya iizuka (@hiroyaiizuka) #hard #deep
+  
+  ### Question
+  
+  Implement a type DeepPick, that extends Utility types `Pick`.
+  A type takes two arguments.
+  
+  
+  For example:
+  
+  ```
+  
+  type obj = {
+    name: 'hoge', 
+    age: 20,
+    friend: {
+      name: 'fuga',
+      age: 30,
+      family: {
+        name: 'baz',  
+        age: 1 
+      }
+    }
+  }
+  
+  type T1 = DeepPick<obj, 'name'>   // { name : 'hoge' }
+  type T2 = DeepPick<obj, 'name' | 'friend.name'>  // { name : 'hoge' } & { friend: { name: 'fuga' }}
+  type T3 = DeepPick<obj, 'name' | 'friend.name' |  'friend.family.name'>  // { name : 'hoge' } &  { friend: { name: 'fuga' }} & { friend: { family: { name: 'baz' }}}
+  
+  ```
+  
+  > View on GitHub: https://tsch.js.org/956
+*/
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type Obj = {
+  a: number;
+  b: string;
+  c: boolean;
+  obj: {
+    d: number;
+    e: string;
+    f: boolean;
+    obj2: {
+      g: number;
+      h: string;
+      i: boolean;
+    };
+  };
+  obj3: {
+    j: number;
+    k: string;
+    l: boolean;
+  };
+};
+
+type cases = [
+  Expect<Equal<DeepPick<Obj, "">, unknown>>,
+  Expect<Equal<DeepPick<Obj, "a">, { a: number }>>,
+  Expect<
+    Equal<DeepPick<Obj, "a" | "obj.e">, { a: number } & { obj: { e: string } }>
+  >,
+  Expect<
+    Equal<
+      DeepPick<Obj, "a" | "obj.e" | "obj.obj2.i">,
+      { a: number } & { obj: { e: string } } & { obj: { obj2: { i: boolean } } }
+    >
+  >
+];
+
+/* _____________ Original Code Here _____________ */
+
+// type DeepPick = any;
+
+/* _____________ Your Code Here _____________ */
+
+type UnionToIntersection<U> = (U extends U ? (arg: U) => any : never) extends (
+  arg: infer P
+) => any
+  ? P
+  : never;
+
+type DeepPick<T, P> = UnionToIntersection<
+  P extends `${infer Key}.${infer Rest}`
+    ? Key extends keyof T
+      ? {
+          [K in Key]: DeepPick<T[Key], Rest>;
+        }
+      : never
+    : P extends keyof T
+    ? { [K in P]: T[K] }
+    : never
+>;
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/956/answer
+  > View solutions: https://tsch.js.org/956/solutions
+  > More Challenges: https://tsch.js.org
+*/
