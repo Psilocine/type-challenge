95c578cf95f922cdc3d024122dc5ca456b36b6b0
PsiloLau <psilolau@foxmail.com>
2022-04-28 14:54:45 +0800

    docs: daily type learning

---
+++ 3_hard/6141_BinaryToDecimal.ts
@@ -0,0 +1,52 @@
+/*
+  6141 - Binary to Decimal
+  -------
+  by wotsushi (@wotsushi) #hard #math
+  
+  ### Question
+  
+  Implement `BinaryToDecimal<S>` which takes an exact string type `S` consisting 0 and 1 and returns an exact number type corresponding with `S` when `S` is regarded as a binary.
+  You can assume that the length of `S` is equal to or less than 8 and `S` is not empty.
+  
+  ```ts
+  type Res1 = BinaryToDecimal<'10'>; // expected to be 2
+  type Res2 = BinaryToDecimal<'0011'>; // expected to be 3
+  ```
+  
+  > View on GitHub: https://tsch.js.org/6141
+*/
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<BinaryToDecimal<"10">, 2>>,
+  Expect<Equal<BinaryToDecimal<"0011">, 3>>,
+  Expect<Equal<BinaryToDecimal<"00000000">, 0>>,
+  Expect<Equal<BinaryToDecimal<"11111111">, 255>>,
+  Expect<Equal<BinaryToDecimal<"10101010">, 170>>
+];
+
+/* _____________ Original Code Here _____________ */
+
+// type BinaryToDecimal<S extends string> = any;
+
+/* _____________ Your Code Here _____________ */
+
+type ExpandTwo<T extends string[]> = [...T, ...T];
+
+type BinaryToDecimal<
+  S extends string,
+  Res extends string[] = []
+> = S extends `${infer L}${infer NS}`
+  ? L extends "1"
+    ? BinaryToDecimal<NS, [...ExpandTwo<Res>, "0"]>
+    : BinaryToDecimal<NS, ExpandTwo<Res>>
+  : Res["length"];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/6141/answer
+  > View solutions: https://tsch.js.org/6141/solutions
+  > More Challenges: https://tsch.js.org
+*/
